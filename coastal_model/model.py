5957321e2a3e940f187e1fc834679889006720b4
Henry Rong <henryrong001@gmail.com>
2024-03-08 04:53:31 +0000

    merged original mesa abm and meso-geo abm. added simplistic stochastic storm

--- coastal_model/model.py
+++ coastal_model/model.py
@@ -9,6 +9,46 @@ from shapely.geometry import Point
 
 from .space import CoastalArea
 
+# define functions
+
+migration_cost = 10 # £1k
+
+def call_sea_level(model):
+    return model.sea_level
+
+def call_migration_count(model):
+    return model.migration_count
+
+def damage(level):
+    if level < 0:
+        return 0
+    else:
+        # return (1/3*math.log(math.exp(-9/5) + level*3)+3/5)*10 # cost of damage in £1k, assuming 100 sq m house
+        return level*10
+
+def expected_utility(income, savings, flood_preparedness, elevation, amenity, sea_level):
+    """Calculate the expected utility of the household."""
+    sums = savings + amenity + income
+
+    flood_damage = damage(sea_level)
+
+    # Adapt
+    if sea_level < elevation:
+        nothing = sums
+    else:
+        nothing = sums - flood_damage
+    
+    adapt = sums + damage(flood_preparedness) - flood_damage
+    
+    # Migrate
+    migrate = sums - migration_cost
+
+    utility = {nothing:'nothing', adapt:'adapt', migrate:'migrate'}
+
+    # returns string of the action with the highest utility
+    return utility.get(max(utility))
+
+
 
 class Person(mg.GeoAgent):
     MOBILITY_RANGE_X = 0.0
@@ -18,6 +58,14 @@ class Person(mg.GeoAgent):
         super().__init__(unique_id, model, geometry, crs)
         self.img_coord = img_coord
 
+        # Randomise income, consumption, and savings
+        self.income = np.random.normal(30, 10) # £1k, post tax and consumption
+        self.savings = np.random.normal(30, 10) # £1k
+        self.flood_preparedness = np.random.normal(0, 0.5) # metres of flood barrier
+        self.elevation = 0 # initial height above sea level in metres
+        self.amenity = np.random.normal(100,50) # amenity value of the location in £1k
+        
+
     def set_random_world_coord(self):
         world_coord_point = Point(self.model.space.population_layer.transform * self.img_coord)
         random_world_coord_x = world_coord_point.x + np.random.uniform(-self.MOBILITY_RANGE_X, self.MOBILITY_RANGE_X)
@@ -25,6 +73,22 @@ class Person(mg.GeoAgent):
         self.geometry = Point(random_world_coord_x, random_world_coord_y)
 
     def step(self):
+        # dictionary of expected utility
+        utility_case = expected_utility(self.income, self.savings, self.flood_preparedness, self.elevation, self.amenity, self.model.sea_level)
+
+        match utility_case:
+            case 'nothing':
+                if self.model.sea_level > self.elevation + self.flood_preparedness: # flooded
+                    self.amenity -= damage(self.model.sea_level) 
+                pass
+            case 'adapt':
+                self.adapt()
+            case 'migrate':
+                self.migrate()
+
+    def migrate(self):
+
+        self.model.migration_count += 1
         neighborhood = self.model.space.population_layer.get_neighborhood(self.img_coord, moore=True)
         found = False
         while neighborhood and not found:
@@ -38,6 +102,10 @@ class Person(mg.GeoAgent):
                 self.img_coord = next_img_coord
                 self.set_random_world_coord()
 
+    def adapt(self):
+        self.flood_preparedness += 0.5
+        self.savings -= 10
+
 
 class Population(mesa.Model):
     def __init__(
@@ -47,14 +115,23 @@ class Population(mesa.Model):
         world_zip_file="data/clip2.zip",
     ):
         super().__init__()
+        self.step_count = 0
         self.space = CoastalArea(crs="epsg:4326")
         self.space.load_data(population_gzip_file, sea_zip_file, world_zip_file)
         pixel_size_x, pixel_size_y = self.space.population_layer.resolution
         Person.MOBILITY_RANGE_X = pixel_size_x / 2
         Person.MOBILITY_RANGE_Y = pixel_size_y / 2
-
         self.schedule = mesa.time.RandomActivation(self)
         self._create_agents()
+        self.migration_count = 0
+
+        # sea level rise
+
+        self.sea_level = 0 # max sea level in year timestep. unit is metres
+        self.datacollector = mesa.DataCollector(
+            model_reporters={"Sea Level": call_sea_level, "Migration Count": call_migration_count},
+            agent_reporters={"Adaptation":"flood_preparedness"},
+        )
 
     def _create_agents(self):
         num_agents = 0
@@ -76,5 +153,11 @@ class Population(mesa.Model):
                         self.space.add_agents(person)
                         self.schedule.add(person)
 
-    def step(self):
+    def step(self): #from 2010 to 2080
+
+        self.step_count +=1
+        stochastic_storm = np.random.choice(a = [0, 2 + self.step_count],p = [(1 - (1+self.step_count)/100),(1+self.step_count)/100])
+        self.sea_level += self.step_count/1000 + stochastic_storm # m/year # includes a flood variation
+        self.datacollector.collect(self)
         self.schedule.step()
+        self.sea_level -= stochastic_storm
--- coastal_model/server.py
+++ coastal_model/server.py
@@ -11,18 +11,17 @@ class NumAgentsElement(mesa.visualization.TextElement):
         super().__init__()
 
     def render(self, model):
-        return f"Number of Agents: {len(model.space.agents)}"
+        return f"Number of Agents: {len(model.space.agents)}  Migration Count: {model.migration_count}"
 
 
 def agent_portrayal(agent):
     if isinstance(agent, mg.GeoAgent):
         if isinstance(agent.geometry, Point):
-            return {
-                "stroke": False,
-                "color": "Green",
-                "radius": 2,
-                "fillOpacity": 0.3,
-            }
+            portrayal = {"stroke": False,"color": "Green", "fillOpacity": 0.3, "radius": 2}
+            portrayal["color"] = "Red" if agent.flood_preparedness < 0.5 else "Green"
+            portrayal["radius"] = 1 if agent.flood_preparedness < 0.5 else 2
+            return portrayal
+        
         elif isinstance(agent.geometry, Polygon):
             return {
                 "fillColor": "Blue",
@@ -34,7 +33,7 @@ def agent_portrayal(agent):
 
 geospace_element = mg.visualization.MapModule(agent_portrayal,map_width=700)
 num_agents_element = NumAgentsElement()
+chart1 = mesa.visualization.ChartModule([{"Label": "Sea Level", "Color": "Black"}], data_collector_name="datacollector")
+chart2 = mesa.visualization.ChartModule([{"Label": "Migration Count", "Color": "Red"}], data_collector_name="datacollector")
 
-server = mesa.visualization.ModularServer(
-    Population, [geospace_element, num_agents_element], "Population Model"
-)
+server = mesa.visualization.ModularServer(Population, [geospace_element, num_agents_element, chart1,chart2], "Population Model")
